9b9b92232cc2ea1b61fbe9f39f69d75384589623
Rakeshraj K P <rakeshrajkp.work@gmail.com>
2024-11-29 18:09:36 +0530

    User Aunthentication in Login and Signup page

---
+++ utils/auth.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyA67b6V6dL3yfPkhSwor1g7DTrkQ37Y1YU";
+
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  const token = response.data.idToken;
+  return token;
+}
+
+export function createUser(email, password) {
+  return authenticate("signUp", email, password);
+
+  return token;
+}
+
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
+}
